a10959f927c51f5ce798b3baee5706b0ca11a6e2
Gaurav Shrestha <gaurav.shrestha04@gmail.com>
2019-11-02 13:46:32 +0530

    setup client side routing

---
+++ client/src/components/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <div>
+          <Header />
+          <Route path='/' exact component={StreamList}>
+            StreamList{" "}
+          </Route>
+          <Route path='/streams/new' component={StreamCreate}>
+            StreamCreate{" "}
+          </Route>
+          <Route path='/streams/edit' component={StreamEdit}>
+            StreamEdit{" "}
+          </Route>
+          <Route path='/streams/show' component={StreamShow}>
+            StreamShow{" "}
+          </Route>
+          <Route path='/streams/delete' component={StreamDelete}>
+            StreamDelete{" "}
+          </Route>
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
